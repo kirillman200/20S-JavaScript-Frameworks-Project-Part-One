f0c2a1a0fa07357a87296adc00698b1598d6c679
Kiril Mankovskyi <200412952@student.georgianc.on.ca>
2020-07-09 17:42:14 -0400

    Final

--- routes/carts.js
+++ routes/carts.js
@@ -21,12 +21,12 @@ module.exports = router => {
     var cart = new Cart(req.session.cart ? req.session.cart : {items: {}});     
     meme.findById(productId, function(err, meme){
         if(err){
-          req.flash('Error', 'Meme was not added to the cart');
+          req.flash('Error', meme.title,'Meme was not added to the cart');
             return res.redirect('/memes');
         }
         cart.add(meme, productId);
         req.session.cart = cart;
-        req.flash('success', 'Meme Was added to the cart successfully');
+        req.flash('success', meme.title, 'added to the cart successfully');
         console.log(req.session.cart);
         res.redirect('/memes');
     })
@@ -37,8 +37,8 @@ module.exports = router => {
   var cart = new Cart(req.session.cart ? req.session.cart : {});
 
   cart.deleteItem(productId);
+  req.flash('success', meme.title, 'Meme Was Deleted Successfully');
   req.session.cart = cart;
-  req.flash('success', 'Meme Was Deleted Successfully');
   res.redirect('/cart/')
  });
 
